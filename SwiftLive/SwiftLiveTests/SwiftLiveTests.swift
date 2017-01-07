fb67b35194dc729f12f0437c9b667f1698a23fb7
apple <1538949997@qq.com>
2017-01-07 22:32:42 +0800

    初始化项目

---
+++ SwiftLive/SwiftLiveTests/SwiftLiveTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftLiveTests.swift
+//  SwiftLiveTests
+//
+//  Created by apple on 2017/1/7.
+//  Copyright © 2017年 Doubuy. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLive
+
+class SwiftLiveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
